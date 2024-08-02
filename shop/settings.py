de3255349b2a51ef7c38228f30b32badb5dc108a
timur <stukantimur811@gmail.com>
2024-08-02 14:59:09 +0300

    ff

--- shop/settings.py
+++ shop/settings.py
@@ -87,11 +87,11 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "test-shop",
-        "USER": "postgres",
-        "PASSWORD": "timurrum47",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        'NAME': os.environ.get('POSTGRES_DB'),
+        "USER": os.environ.get('POSTGRES_USER'),
+        "PASSWORD": os.environ.get('POSTGRES_PASSWORD'),
+        "HOST": os.environ.get('POSTGRES_HOST'),
+        "PORT": os.environ.get('POSTGRES_PORT'),
     }
 }
 
